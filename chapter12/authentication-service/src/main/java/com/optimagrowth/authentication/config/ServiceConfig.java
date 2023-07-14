125871d040a498e9236b569b8638ccc8c4763435
darshan <csdhruv007@gmail.com>
2023-07-14 20:33:31 +0530

    message

--- chapter12/authentication-service/src/main/java/com/optimagrowth/authentication/config/ServiceConfig.java
+++ chapter12/authentication-service/src/main/java/com/optimagrowth/authentication/config/ServiceConfig.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+
+
+
+
 @Component
 @Configuration
 public class ServiceConfig {
